dd5a36fb95ed719962395999d7f59422edfda4fe
nakagam3 <botch.10no@gmail.com>
2022-12-02 01:03:36 +0900

    react部分は不要なので削除

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,6 @@
 // Polyfill "window.fetch" used in the React component.
 import 'whatwg-fetch'
 
-// Extend Jest "expect" functionality with Testing Library assertions.
-import '@testing-library/jest-dom'
-
 import { server } from './src/mocks/server'
 
 beforeAll(() => {
